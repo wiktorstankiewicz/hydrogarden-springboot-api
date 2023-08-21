a9cbd52990145ff4c898798e5586281b79d4d91d
Wiktor <266597@student.pwr.edu.pl>
2023-08-22 00:40:24 +0200

    Added empty HydroponicController

---
+++ src/main/java/com/hydrogarden/server/controllers/HydroponicController.java
@@ -0,0 +1,11 @@
+package com.hydrogarden.server.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hydroponic")
+public class HydroponicController {
+
+}
