cdb0ea9c84b716ca517e844f9dccdc4c10d9d950
Wiktor <266597@student.pwr.edu.pl>
2023-08-21 23:08:22 +0200

    Added missing cookie exception handler

--- src/main/java/com/hydrogarden/server/controllers/AuthController.java
+++ src/main/java/com/hydrogarden/server/controllers/AuthController.java
@@ -10,6 +10,8 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -32,30 +35,31 @@ public class AuthController {
     private final Logger logger = Logger.getLogger(AuthController.class.getName());
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterRequestEntity registerRequestEntity){
+    public ResponseEntity<String> register(@Valid @RequestBody RegisterRequestEntity registerRequestEntity) {
         String encodedPassword = passwordEncoder.encode(registerRequestEntity.getPassword());
         User user = User.builder().password(encodedPassword).username(registerRequestEntity.getUsername()).role(Role.USER).build();
-        try{
+        try {
             User addedUser = userRepository.save(user);
-        }catch(Exception e){
-            return new ResponseEntity<>("User already exists",HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("User already exists", HttpStatus.BAD_REQUEST);
         }
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody LoginRequestEntity loginRequestEntity, HttpServletResponse response){
+    public ResponseEntity<String> login(@Valid @RequestBody LoginRequestEntity loginRequestEntity, HttpServletResponse response) {
         String username = loginRequestEntity.getUsername();
         String password = loginRequestEntity.getPassword();
 
 
         User user = userRepository.findByUsername(username);
 
-        if(passwordEncoder.matches(password,user.getPassword())){
-            String token = jwtService.generateToken(new HashMap<>(),user);
-            Cookie jwtCookie = new Cookie("Authorization",token);
+        if (passwordEncoder.matches(password, user.getPassword())) {
+            String token = jwtService.generateToken(new HashMap<>(), user);
+            Cookie jwtCookie = new Cookie("Authorization", token);
             jwtCookie.setSecure(true);
             jwtCookie.setHttpOnly(true);
             jwtCookie.setMaxAge(1000);
@@ -63,25 +67,18 @@ public class AuthController {
             response.addCookie(jwtCookie);
             return new ResponseEntity<>(HttpStatus.OK);
         }
-        return new ResponseEntity<>("Password or username is not correct",HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("Password or username is not correct", HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response){
-        if(request.getCookies() == null){
-            return ResponseEntity.badRequest().body("Authorization cookie not found");
-        }
-        Cookie authCookie = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("Authorization")).findFirst().orElse(null);
-        if(authCookie != null){
-            Cookie jwtCookie = new Cookie("Authorization","");
-            jwtCookie.setSecure(true);
-            jwtCookie.setHttpOnly(true);
-            jwtCookie.setMaxAge(0);
-            jwtCookie.setPath("/");
-            response.addCookie(jwtCookie);
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.badRequest().body("Authorization cookie not found");
+    public ResponseEntity<String> logout(@Validated @NotNull @NotBlank @CookieValue("Authorization") String authCookie, HttpServletRequest request, HttpServletResponse response) {
+        Cookie jwtCookie = new Cookie("Authorization", "");
+        jwtCookie.setSecure(true);
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setMaxAge(0);
+        jwtCookie.setPath("/");
+        response.addCookie(jwtCookie);
+        return ResponseEntity.ok().build();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -96,4 +93,16 @@ public class AuthController {
         });
         return errors;
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingRequestCookieException.class)
+    public Map<String,String> handleMissingCookieExceptions(
+            MissingRequestCookieException ex
+    ){
+
+        Map<String,String> errors = new HashMap<>();
+        errors.put("error",ex.getCookieName() + " cookie is missing");
+        return errors;
+    }
+
 }
